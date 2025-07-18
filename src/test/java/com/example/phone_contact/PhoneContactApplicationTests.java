9aaa5acb47a49d1bd1ac623450a4d660cf14fc0d
MitsoaAllan <hei.mitsoa@gmail.com>
2025-07-18 22:03:22 +0300

    test: add GET /ping and GET /home

--- src/test/java/com/example/phone_contact/PhoneContactApplicationTests.java
+++ src/test/java/com/example/phone_contact/PhoneContactApplicationTests.java
@@ -2,15 +2,26 @@ package com.example.phone_contact;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import com.example.phone_contact.endpoint.rest.controller.HealthRestController;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class PhoneContactApplicationTests {
+    private final HealthRestController subject = new HealthRestController();
 
     @Test
-    void contextLoads() {
-        assertEquals(1,1);
+    void test_health_rest_controller_with_ping() {
+        String expected = subject.ping();
+
+        assertEquals("pong", expected);
+    }
+
+    @Test
+    void test_health_rest_controller_with_home() {
+        String expected = subject.home();
+
+        assertEquals("Hello World", expected);
     }
 
 }
